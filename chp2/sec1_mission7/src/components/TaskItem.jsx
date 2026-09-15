6e92c13fb02da4cadf3e7955f428694f29ef05de
rukadeveloper <kansaki135@gmail.com>
2026-09-15 21:46:33 +0900

    two

--- chp2/sec1_mission7/src/components/TaskItem.jsx
+++ chp2/sec1_mission7/src/components/TaskItem.jsx
@@ -1,22 +1,29 @@
-﻿import { useState, useEffect } from "react"
+﻿import { useState, useEffect, useRef } from "react"
+import { MdDelete, MdRadioButtonChecked, MdRadioButtonUnchecked } from "react-icons/md"
 
 export default function TaskItem({ state, toggle, updatePriority, deleteItem }) {
     const [selected, setSelected] = useState('medium')
 
     useEffect(() => {
         updatePriority(state.id, selected)
-    },[selected])
-    
+    }, [selected])
+
     return (
-        <div style={{ width: '500px', display: 'flex', alignItems: 'center' }}>
-            <input type="checkbox" checked={state.isComplete} onChange={() => toggle(state.id)} />
-            <p style={{ marginTop: '14px', marginLeft: '10px' }}>{state.input}</p>
-            <select style={{ marginLeft: '10px'}} value={state.priority} onChange={(e) => setSelected(e.target.value)} disabled={state.isComplete}>
+        <div className="taskItem" style={{ width: '500px', display: 'flex', alignItems: 'center' }}>
+            <input type="checkbox" checked={state.isComplete} onChange={() => toggle(state.id)} style={{ display: 'none' }} />
+            {state.isComplete ? <MdRadioButtonChecked onClick={() => toggle(state.id)} style={{ opacity: '0.5' }} /> : <MdRadioButtonUnchecked onClick={() => toggle(state.id)} />}
+            <p style={{ marginTop: '12px', marginLeft: '10px', opacity: state.isComplete ? '0.5' : '1' }} className={`${state.isComplete ? 'lineThrough' : ''}`}>{state.input}</p>
+            <select style={{ marginLeft: '10px' }} value={state.priority} onChange={(e) => setSelected(e.target.value)} disabled={state.isComplete}>
                 <option value="low">Low</option>
                 <option value="medium">Medium</option>
                 <option value="high">High</option>
             </select>
-            <button style={{ marginLeft: '20px'}} onClick={() => {deleteItem(state.id)}}>삭제</button>
+            <button style={{ marginLeft: '20px', backgroundColor: 'transparent', border: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                onClick={() => { deleteItem(state.id) }}
+                disabled={state.isComplete}
+            >
+                <MdDelete />
+            </button>
         </div>
     )
 }
